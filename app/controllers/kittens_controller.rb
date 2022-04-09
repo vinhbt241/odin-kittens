29b6046e40b855be359a25f11c3e8be6a7191dc3
Vinh <vinhbuith32412000@gmail.com>
2022-04-09 10:09:44 +0700

    Complete functions in KittensController

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,29 +1,50 @@
 class KittensController < ApplicationController
   def index 
-
+    @kittens = Kitten.all
   end
 
   def show 
-
+    @kitten = Kitten.find(params[:id])
   end
   
   def new 
-
+    @kitten = Kitten.new
   end
 
   def create 
+    @kitten = Kitten.new(kitten_params)
 
+    if @kitten.save 
+      redirect_to @kitten
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit 
-
+    @kitten = Kitten.find(params[:id])
   end
 
   def update 
-    
+    @kitten = Kitten.find(params[:id])
+
+    if @kitten.update(kitten_params)
+      redirect_to @kitten
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy 
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy 
     
+    redirect_to root_path
+  end
+
+  private 
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
   end
 end
